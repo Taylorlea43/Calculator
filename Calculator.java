4c641788267eefd067f6ee2c5680d830e5e90614
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-08 11:44:42 -0500

    take in two numbers and a or s for addition or subtraction

--- Calculator.java
+++ Calculator.java
@@ -12,8 +12,23 @@ public class Calculator {
         System.out.println("Please enter the second number: ");
         int numTwo = scanner.nextInt();
 
+        //ask users if they want to add or subtract
+        System.out.println("Please enter 'a' if you would like to add the two numbers or 's' if you would like to subtract them: ");
+        char operation = scanner.next().charAt(0);
+
+        int sum = -1;
+        
+        if(operation == 'a'){
         //Add numbers together and print result
-        int sum = numOne + numTwo;
+        sum = numOne + numTwo;
         System.out.print(sum);
+        }else if(operation == 's'){
+            //subtract numbers and print result
+        sum = numOne - numTwo;
+        System.out.print(sum);
+        }else{
+            System.out.println("invalid option");
+            return;
+        }
     }
 }
