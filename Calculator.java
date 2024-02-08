adb26896640d33221a136d1a5d60c7cea0abcb3f
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-08 11:30:40 -0500

    User inserts two numbers and their sum is returned

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,19 @@
+import java.util.*;
 public class Calculator {
 
     public static void main(String[] args) {
-        
+        //create new scanner to get user input
+        Scanner scanner = new Scanner(System.in);
+
+        //ask user to enter the numbers
+        System.out.println("Please enter the first number: ");
+        int numOne = scanner.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int numTwo = scanner.nextInt();
+
+        //Add numbers together and print result
+        int sum = numOne + numTwo;
+        System.out.print(sum);
     }
 }
